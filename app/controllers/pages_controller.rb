eb7cceed6430a05c12063aef3bef58bce9c6871d
Dean DeHart <DeHartDean@gmail.com>
2023-01-01 17:03:56 -0500

    Refactor nav to partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home; end
 
   def pathfindout; end
+
+  def source
+    redirect_to 'https://github.com/Deanout/turbopath'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "pathfindout", to: "pages#pathfindout"
+  get 'pathfindout', to: 'pages#pathfindout'
+  get 'source', to: 'pages#source'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
